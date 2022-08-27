a14cfa94553fb5cf724d1ab9dff64f9f8abdd2cc
Amil Silahic <jinobie02@gmail.com>
2022-08-27 09:52:14 -0600

    changing header conditionally for mange expense

--- screens/manage-expense-screen.js
+++ screens/manage-expense-screen.js
@@ -1,6 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 
-const ManageExpense = () => {
+import { useLayoutEffect } from "react";
+
+const ManageExpense = ({ route, navigation }) => {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>This is the ManageExpense screen</Text>
